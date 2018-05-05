5ac043542916919a6c70add46d1591b3360bdbdc
Josh <Rdeckj@gmail.com>
2018-05-05 11:50:17 -0400

    Validate false when out of stock

--- test/StockKeeperTest.java
+++ test/StockKeeperTest.java
@@ -11,4 +11,12 @@ public class StockKeeperTest {
         assertTrue(stockKeeper.isChipsInStock());
         assertTrue(stockKeeper.isCandyInStock());
     }
+
+    @Test
+    public void productInStockWhen0ReturnFalse() {
+        StockKeeper stockKeeper = new StockKeeper(0, 0, 0);
+        assertFalse(stockKeeper.isPopInStock());
+        assertFalse(stockKeeper.isChipsInStock());
+        assertFalse(stockKeeper.isCandyInStock());
+    }
 }
