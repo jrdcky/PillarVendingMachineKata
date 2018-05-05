6224327646d4047e30b3ef28f1ba744ca2686121
Josh <Rdeckj@gmail.com>
2018-05-05 11:35:01 -0400

    Display EXACT CHANGE ONLY when appropriate

--- src/Display.java
+++ src/Display.java
@@ -1,10 +1,26 @@
 public class Display {
     static final String INSERT_COIN = "INSERT COIN";
+    static final String EXACT_CHANGE_ONLY = "EXACT CHANGE ONLY";
+    private boolean requireExactChange;
+
+    public Display(boolean requireExactChange) {
+        this.requireExactChange = requireExactChange;
+    }
 
     public String getPrompt(double amountInserted) {
         if(amountInserted == 0) {
-            return INSERT_COIN;
+            if(requireExactChange) {
+                return EXACT_CHANGE_ONLY;
+            }
+            else {
+                return INSERT_COIN;
+            }
         }
         return "$" + String.valueOf(amountInserted);
     }
+
+    public void setRequireExactChange(boolean requireExactChange) {
+        this.requireExactChange = requireExactChange;
+    }
+
 }
--- test/DisplayTest.java
+++ test/DisplayTest.java
@@ -5,14 +5,22 @@ import static org.junit.Assert.*;
 public class DisplayTest {
 
     @Test
-    public void getDisplayNoCoinsEqualsInsertCoin() {
-        Display display = new Display();
+    public void getPromptNoCoinsEqualsInsertCoin() {
+        Display display = new Display(false);
         assertEquals(Display.INSERT_COIN, display.getPrompt(0));
     }
 
     @Test
-    public void getDisplayWithCoinsDisplaysValue() {
-        Display display = new Display();
+    public void getPromptWithCoinsDisplaysValue() {
+        Display display = new Display(false);
         assertEquals("$0.25", display.getPrompt(.25));
+        assertEquals("$1.25", display.getPrompt(1.25));
+        assertEquals("$0.05", display.getPrompt(.05));
+    }
+
+    @Test
+    public void getPromptNoCoinsExactChangeDisplayExactChange() {
+        Display display = new Display(true);
+        assertEquals(Display.EXACT_CHANGE_ONLY, display.getPrompt(0));
     }
 }
