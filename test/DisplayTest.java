c5dc26534a5197feb652b87de86aeaf24fd41169
Josh <Rdeckj@gmail.com>
2018-05-05 11:37:47 -0400

    Refactor tests

--- test/DisplayTest.java
+++ test/DisplayTest.java
@@ -1,26 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DisplayTest {
 
+    Display display;
+
+    @Before
+    public void setUp() {
+        display = new Display(false);
+    }
+
     @Test
     public void getPromptNoCoinsEqualsInsertCoin() {
-        Display display = new Display(false);
         assertEquals(Display.INSERT_COIN, display.getPrompt(0));
     }
 
     @Test
     public void getPromptWithCoinsDisplaysValue() {
-        Display display = new Display(false);
         assertEquals("$0.25", display.getPrompt(.25));
         assertEquals("$1.25", display.getPrompt(1.25));
+        display.setRequireExactChange(true);
         assertEquals("$0.05", display.getPrompt(.05));
+        assertEquals("$1.05", display.getPrompt(1.05));
     }
 
     @Test
     public void getPromptNoCoinsExactChangeDisplayExactChange() {
-        Display display = new Display(true);
+        display.setRequireExactChange(true);
         assertEquals(Display.EXACT_CHANGE_ONLY, display.getPrompt(0));
     }
 }
